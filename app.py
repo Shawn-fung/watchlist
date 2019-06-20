5cea5b49c73e3559101b07b5edc585143ef83b1f
feng <www.1208236913@qq.com>
2019-06-21 00:01:33 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -18,11 +18,33 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的
 
 @app.route('/')
 def index():
-    user = User.query.first()  #从数据库读取用户信息
+    #user = User.query.first()  #从数据库读取用户信息
     movies = Movie.query.all()  #从数据库  电影记录
-    return render_template('index.html',user=user,movies=movies)
+    return render_template('index.html',movies=movies)
 
 
+'''
+自动被调用
+使用 app.errorhandler() 装饰器注册一个错误处理函数，它的作用和视图函数类似，当 404 错误发生时，这个函数会被触发，返回值会作为响应主体返回给客户端：
+'''
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+
+    return render_template('404.html'), 404  # 返回模板和状态码
+
+
+'''
+对于多个模板内都需要使用的变量，我们可以使用 app.context_processor 装饰器注册一个模板上下文处理函数，如下所示：
+'''
+#自定义模板上下文处理函数
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典，等同于return {'user': user}
+'''
+这个函数返回的变量（以字典键值对的形式）将会自动统一注入到所有模板的上下文环境中，因此可以直接在模板中使用。
+现在我们可以删除 404 错误处理函数和主页视图函数中的 user 变量定义，并删除在 render_template() 函数里传入的关键字参数
+'''
 
 
 # 在扩展类实例化前加载配置
